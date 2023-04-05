7e038ffa744900dd7f6b0323d54d8bd44b8439a5
heung115 <heung115@naver.com>
2023-04-05 15:59:52 +0900

    문제 입력부분을 잘못 처리

--- 4153/4153.c
+++ 4153/4153.c
@@ -2,9 +2,26 @@
 
 int main() {
     int x, y, z;
+    int ans[100] = {0};
+    int i = 0;
+    int k = 0;
     scanf("%d %d %d", &x, &y, &z);
-    if (x * x + y * y == z * z)
-        printf("right");
-    else
-        printf("wrong");
+    while (1) {
+        if (x == 0 && y == 0 && z == 0) {
+            ans[i] = -1;
+            break;
+        } else if (x * x + y * y == z * z) {
+            ans[i++] = 1;
+        } else {
+            ans[i++] = 0;
+        }
+        scanf("%d %d %d", &x, &y, &z);
+    }
+    while (ans[k] != -1) {
+        if (ans[k] == 1)
+            printf("right\n");
+        else
+            printf("wrong\n");
+        k++;
+    }
 }
