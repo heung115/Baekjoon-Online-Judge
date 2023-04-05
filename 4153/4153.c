a092d40a2c66e1153b52aaea9c35b0579ebf89c3
heung115 <heung115@naver.com>
2023-04-05 16:28:52 +0900

    4153

--- 4153/4153.c
+++ 4153/4153.c
@@ -2,21 +2,42 @@
 
 int main() {
     int x, y, z;
+    int max;
     int ans[100] = {0};
     int i = 0;
-    int k = 0;
-    scanf("%d %d %d", &x, &y, &z);
     while (1) {
+        scanf("%d %d %d", &x, &y, &z);
+        max = x;
         if (x == 0 && y == 0 && z == 0) {
             ans[i] = -1;
             break;
-        } else if (x * x + y * y == z * z) {
-            ans[i++] = 1;
-        } else {
-            ans[i++] = 0;
         }
-        scanf("%d %d %d", &x, &y, &z);
+        if (y > max) {
+            max = y;
+        }
+        if (z > max) {
+            max = z;
+        }
+        if (max == x) {
+            if (y * y + z * z == x * x)
+                ans[i] = 1;
+            else
+                ans[i] = 0;
+        } else if (max == y) {
+            if (y * y == z * z + x * x)
+                ans[i] = 1;
+            else
+                ans[i] = 0;
+        } else {
+            if (x * x + y * y == z * z)
+                ans[i] = 1;
+            else
+                ans[i] = 0;
+            ;
+        }
+        i++;
     }
+    int k = 0;
     while (ans[k] != -1) {
         if (ans[k] == 1)
             printf("right\n");
@@ -24,4 +45,5 @@ int main() {
             printf("wrong\n");
         k++;
     }
+    return 0;
 }
